f6e2873641301e0a5603dd880760eb6582efc457
Jose <mayol.jose.f@gmail.com>
2018-08-22 19:28:49 -0300

    Agrega "_" al idProducto para coincidir con el mapper del dto al model en api.

--- src/main/java/sic/modelo/RenglonPedido.java
+++ src/main/java/sic/modelo/RenglonPedido.java
@@ -10,12 +10,11 @@ import lombok.EqualsAndHashCode;
 public class RenglonPedido implements Serializable {
 
     private long id_RenglonPedido;   
-    private Producto producto;
     private BigDecimal cantidad;
     private BigDecimal descuento_porcentaje;
     private BigDecimal descuento_neto;
     private BigDecimal subTotal;
-    private long idProducto;
+    private long id_Producto;
     private String codigoProducto;
     private String descripcionProducto;
     private BigDecimal precioDeListaProducto; 
