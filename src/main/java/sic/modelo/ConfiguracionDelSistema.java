f8689f18fb4e3bc06b4e7bd6172012976e551ada
Mayol Jose <mayol.jose.f@gmail.com>
2019-05-08 14:06:01 -0300

    Modifica la vista de Configuracion del Sistema por los cambios en el model.

--- src/main/java/sic/modelo/ConfiguracionDelSistema.java
+++ src/main/java/sic/modelo/ConfiguracionDelSistema.java
@@ -7,16 +7,31 @@ import lombok.Data;
 public class ConfiguracionDelSistema implements Serializable {
 
     private long id_ConfiguracionDelSistema;
+
     private boolean usarFacturaVentaPreImpresa;
-    private int cantidadMaximaDeRenglonesEnFactura; 
-    private boolean facturaElectronicaHabilitada;    
-    private byte[] certificadoAfip;    
-    private String firmanteCertificadoAfip;    
+
+    private int cantidadMaximaDeRenglonesEnFactura;
+
+    private boolean facturaElectronicaHabilitada;
+    
+    private byte[] certificadoAfip;
+
+    private boolean existeCertificado;
+
+    private String firmanteCertificadoAfip;
+    
     private String passwordCertificadoAfip;
+
     private int nroPuntoDeVentaAfip;
+
     private boolean emailSenderHabilitado;
+
     private String emailUsername;
+    
     private String emailPassword;
-    private Empresa empresa;
+
+    private long idEmpresa;
+
+    private String nombreEmpresa;
     
 }
