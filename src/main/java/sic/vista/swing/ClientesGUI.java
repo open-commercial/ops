141492d49eeeae67d09e2f436f398abfcec841f9
Mayol Jose <mayol.jose.f@gmail.com>
2020-01-16 23:55:03 -0300

    HOTFIX Corrige el sorting por compra mínima.

--- src/main/java/sic/vista/swing/ClientesGUI.java
+++ src/main/java/sic/vista/swing/ClientesGUI.java
@@ -279,7 +279,7 @@ public class ClientesGUI extends JInternalFrame {
                 criteria.setOrdenarPor("fechaUltimoMovimiento");
                 break;
             case 5:
-                criteria.setOrdenarPor("cliente.bonificacion");
+                criteria.setOrdenarPor("cliente.montoCompraMinima");
                 break;
         }
         int seleccionDireccion = cmbSentido.getSelectedIndex();
@@ -605,7 +605,7 @@ public class ClientesGUI extends JInternalFrame {
 
         panelOrden.setBorder(javax.swing.BorderFactory.createTitledBorder("Ordenar Por"));
 
-        cmbOrden.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "R. Social o Nombre", "Fecha Alta", "Nombre Fantasia", "Saldo C/C", "Ultimo Movimiento C/C", "Bonificación" }));
+        cmbOrden.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "R. Social o Nombre", "Fecha Alta", "Nombre Fantasia", "Saldo C/C", "Ultimo Movimiento C/C", "Compra Mín." }));
         cmbOrden.addItemListener(new java.awt.event.ItemListener() {
             public void itemStateChanged(java.awt.event.ItemEvent evt) {
                 cmbOrdenItemStateChanged(evt);
