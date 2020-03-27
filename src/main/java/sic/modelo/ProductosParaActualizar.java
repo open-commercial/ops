1be63ad093442aaa49e9080c72313cffc6c10af6
Mayol Jose <mayol.jose.f@gmail.com>
2020-03-26 23:43:05 -0300

    Modifica la clase de productos para actualizar.

--- src/main/java/sic/modelo/ProductosParaActualizar.java
+++ src/main/java/sic/modelo/ProductosParaActualizar.java
@@ -13,13 +13,9 @@ public class ProductosParaActualizar {
     private Long idMedida;
     private Long idRubro;
     private Long idProveedor;
-    private BigDecimal gananciaNeto;
     private BigDecimal gananciaPorcentaje;
-    private BigDecimal ivaNeto;
     private BigDecimal ivaPorcentaje;
     private BigDecimal precioCosto;
-    private BigDecimal precioLista;
-    private BigDecimal precioVentaPublico;
     private BigDecimal porcentajeBonificacionPrecio;
     private Boolean publico;
 
